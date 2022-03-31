b66a9809334d269524498875db7f87b24327f21f
ayawen <52957307+Ayawen01@users.noreply.github.com>
2022-03-31 11:37:06 +0800

    11:37

---
+++ lox-rs/src/tokentype.rs
@@ -0,0 +1,20 @@
+pub enum TokenType {
+    // Single-character tokens.
+    Left_Paren, Right_Paren, Left_Brace, Right_Brace,
+    Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
+  
+    // One or two character tokens.
+    Bang, Bang_Eqlal,
+    Equal, Equal_Equal,
+    Greater, Greater_Equal,
+    Less, Less_Equal,
+  
+    // Literals.
+    Identifier, String, Number,
+  
+    // Keywords.
+    And, Class, Else, False, Fun, For, If, Nil, Or,
+    Print, Return, Super, This, True, Var, While,
+  
+    Eof
+}
