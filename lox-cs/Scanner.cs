1451afc1dfbdd411def85b119ee326ad303d9535
ayawen <52957307+Ayawen01@users.noreply.github.com>
2022-04-01 13:10:58 +0800

    13:10

---
+++ lox-cs/Scanner.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lox_cs
+{
+    internal class Scanner
+    {
+        private string Source { get; }
+        private List<Token> Tokens { get; }
+        private int Start { get; set; }
+        private int Current { get; set; }
+        private int Line { get; set; }
+
+        internal Scanner(string source)
+        {
+            Source = source;
+            Tokens = new List<Token>();
+            Start = 0;
+            Current = 0;
+            Line = 1;
+        }
+
+        internal List<Token> ScanTokens()
+        {
+            while (!IsAtEnd())
+            {
+                Start = Current;
+                ScanToken();
+            }
+
+            Tokens.Add(new Token(TokenType.EOF, null, Line));
+            return Tokens;
+        }
+
+        private void ScanToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsAtEnd()
+        {
+            return Current >= Source.Length;
+        }
+    }
+}
---
+++ lox-cs/Token.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lox_cs
+{
+    internal struct Token
+    {
+        internal TokenType Type { get; }
+        internal object? Literal { get; }
+        internal int Line { get; }
+
+        internal Token(TokenType type, object? literal, int line)
+        {
+            Type = type;
+            Literal = literal;
+            Line = line;
+        }
+
+        public override string ToString() => string.Format("Token {{ Type: {0}, Literal: {1}, Line: {2} }}", Type, Literal, Line);
+    }
+}
