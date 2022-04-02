aeedd767cc41a1af46938b7eeb4b1687350505e5
ayawen <52957307+Ayawen01@users.noreply.github.com>
2022-04-02 12:48:59 +0800

    12:48

--- lox-js/main.js
+++ lox-js/main.js
@@ -1,1 +1,24 @@
-console.log('hello.');
+function main() {
+    const args = Deno.args;
+    if (args.length < 2) {
+        run_file(args[0]);
+    } else if (args.length == 1) {
+        run_prompt();
+    } else {
+        console.log("Usage: lox [script]");
+    }
+}
+
+function run(source) {
+
+}
+
+function run_file(path) {
+    
+}
+
+function run_prompt() {
+
+}
+
+main();
