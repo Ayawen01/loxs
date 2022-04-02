95c322792e664a817f75f29bab93f5803a378184
ayawen <52957307+Ayawen01@users.noreply.github.com>
2022-04-02 15:16:57 +0800

    15:16

--- lox-js/src/main.js
+++ lox-js/src/main.js
@@ -1,3 +1,6 @@
+const readline = require('readline');
+const fs = require('fs');
+
 function main() {
     const args = process.argv;
     if (args.length == 3) {
@@ -5,20 +8,27 @@ function main() {
     } else if (args.length == 2) {
         run_prompt();
     } else {
-        console.log("Usage: lox [script]");
+        console.log('Usage: lox [script]');
     }
 }
 
 function run(source) {
-
+    console.log(source);
 }
 
 function run_file(path) {
-
+    const source = fs.readFileSync(path);
+    run(source.toString());
 }
 
 function run_prompt() {
-
+    readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: '> '
+    }).on('line', line => {
+        run(line);
+    });
 }
 
 main();
