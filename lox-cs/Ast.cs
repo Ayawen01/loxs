41c88bc9f94ea709ccc31320929058baf9b7a216
ayawen <52957307+Ayawen01@users.noreply.github.com>
2022-04-22 12:38:16 +0800

    12:38

---
+++ lox-cs/Ast.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lox_cs
+{
+    internal abstract class Expr
+    {
+        internal interface IVisitor<R>
+        {
+            R VisitAssignExpr(Assign expr);
+            R VisitBinaryExpr(Binary expr);
+            R VisitCallExpr(Call expr);
+            R VisitGetExpr(Get expr);
+            R VisitGroupingExpr(Grouping expr);
+            R VisitLiteralExpr(Literal expr);
+            R VisitLogicalExpr(Logical expr);
+            R VisitSetExpr(Set expr);
+            R VisitSuperExpr(Super expr);
+            R VisitThisExpr(This expr);
+            R VisitUnaryExpr(Unary expr);
+            R VisitVariableExpr(Variable expr);
+        }
+
+        internal abstract R Accept<R>(IVisitor<R> visitor);
+
+        internal class Assign : Expr
+        {
+            internal Token Name { get; }
+            internal Expr Value { get; }
+
+            internal Assign(Token name, Expr value)
+            {
+                Name = name;
+                Value = value;
+            }
+
+            internal override R Accept<R>(IVisitor<R> visitor)
+            {
+                return visitor.VisitAssignExpr(this);
+            }
+        }
+    }
+}
