334ca1b57208fc07bbb9bef6e80a1bd59e5420ce
ayawen <52957307+Ayawen01@users.noreply.github.com>
2022-04-24 13:36:39 +0800

    13:36

---
+++ lox-cs/Interpreter.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lox_cs
+{
+    internal class Interpreter : Expr.IVisitor<object>
+    {
+        internal Interpreter()
+        {
+
+        }
+
+        private object Evaluate(Expr expression)
+        {
+            return expression.Accept(this);
+        }
+
+        private bool IsTruthy(object obj)
+        {
+            if (obj is null)
+                return false;
+            if (obj is bool boolean)
+                return boolean;
+            return true;
+        }
+
+        public object VisitAssignExpr(Expr.Assign expr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitBinaryExpr(Expr.Binary expr)
+        {
+            object left = Evaluate(expr.Left);
+            object right = Evaluate(expr.Right);
+
+            switch (expr.Operator.Type) {
+                case TokenType.GREATER:
+                    return (double)left > (double)right;
+                case TokenType.GREATER_EQUAL:
+                    return (double)left >= (double)right;
+                case TokenType.LESS:
+                    return (double)left < (double)right;
+                case TokenType.LESS_EQUAL:
+                    return (double)left <= (double)right;
+                case TokenType.MINUS:
+                    return (double)left - (double)right;
+                case TokenType.SLASH:
+                    return (double)left / (double)right;
+                case TokenType.STAR:
+                    return (double)left * (double)right;
+                case TokenType.PLUS:
+                    if (left is double && right is double)
+                        return (double)left + (double)right;
+                    if (left is string && right is string)
+                        return (string)left + (string)right;
+                    break;
+            }
+
+            return null;
+        }
+
+        public object VisitCallExpr(Expr.Call expr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitGetExpr(Expr.Get expr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitGroupingExpr(Expr.Grouping expr)
+        {
+            return Evaluate(expr.Expression);
+        }
+
+        public object VisitLiteralExpr(Expr.Literal expr)
+        {
+            return expr.Value;
+        }
+
+        public object VisitLogicalExpr(Expr.Logical expr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitSetExpr(Expr.Set expr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitSuperExpr(Expr.Super expr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitThisExpr(Expr.This expr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitUnaryExpr(Expr.Unary expr)
+        {
+            object right = Evaluate(expr.Right);
+
+            switch (expr.Operator.Type)
+            {
+                case TokenType.MINUS:
+                    return -(double)right;
+                case TokenType.BANG:
+                    return !IsTruthy(right);
+            }
+
+            return null;
+        }
+
+        public object VisitVariableExpr(Expr.Variable expr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
